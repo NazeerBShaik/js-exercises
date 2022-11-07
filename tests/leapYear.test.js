51131eb6b8f6509a8c0ce1d28b672dad511a6682
nazeerbshaik <shaiknazeer1995@gmail.com>
2022-11-07 23:06:38 +0530

    leap year addded

---
+++ tests/leapYear.test.js
@@ -0,0 +1,15 @@
+const leapYear = require('../src/leapYear');
+
+test('divisible by 400 => leap year', () => {
+  expect(leapYear(2000)).toBe(true);
+});
+test('divisible by 100 not 400 => not leap year', () => {
+  expect(leapYear(1900)).toBe(false);
+});
+test('divisible by 4 but not 100 => leap year', () => {
+  expect(leapYear(1996)).toBe(true);
+});
+test('divisible by 4 => not leap year', () => {
+    expect(leapYear(1995)).toBe(false);
+  })
+
